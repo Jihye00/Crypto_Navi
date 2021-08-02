f0e066310c28b6ea9ab894ff1cf77be577eb0078
wwwyyyy <97will@naver.com>
2021-08-02 19:41:39 +0900

    contract setting

---
+++ contracts/interfaces/IERC20WithMetaData.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.5.0;
+
+interface IERC20WithMetadata {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
