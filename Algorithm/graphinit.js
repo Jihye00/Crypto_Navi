a475faf95d736d9025ab458f29ff85bd1f1abcd6
Jihye Seo <68698471+Jihye00@users.noreply.github.com>
2021-08-02 12:07:24 +0900

    Update graphinit.js

--- Algorithm/graphinit.js
+++ Algorithm/graphinit.js
@@ -1,146 +1,145 @@
+// import
 const Math = require("mathjs");
+const safemath = require("safemath");
+// constant variables
 const DUMMY_ADDRESS = '0x0000000000000000000000000000000000000000';
-const DUMMY_CURRENCY = 'MOUND';
 const DUMMY_MARKET = 'COINONE';
+const DUMMY_RATIO = -1
 const dex = [
     'KLAYSWAP',
     'DEFINIX'];
+const KLAYSWAP_FEE = 0.003;
+const DEFINIX_FEE = 0.002;
+const MAX_RATIO = 1.1;
+const MIN_RATIO = 0.9;
+const DUMMY_DEX = 'MOUND';
 
-function Currency(name = DUMMY_CURRENCY, availableSwapList) {
+// given that we have list of cureency to deal with,
+var klay = new Currency('KLAY');
+var bnb = new Currency('BNB');
+var usdt = new Currency('USDT');
+var dai = new Currency('DAI');
+var xrp = new Currency('XRP');
+var btc = new Currency('BTC');
+var six = new Currency('SIX');
+var ksp = new Currency('KSP');
+// const klay = new Currency('KLAY');
+var CurrencyLists = [klay, bnb, usdt, dai]; // xrp, btc, six, ksp
+const MATRIX_SIZE = CurrencyLists.length;
+// Currency Class
+function Currency(name = DUMMY_CURRENCY) {
     this.name = name;
-    this.availableSwapList = availableSwapList;
+    // this.availableSwapList = availableSwapList;
 }
+const DUMMY_CURRENCY = new Currency('KLAY');
 
-function Swap(address = DUMMY_ADDRESS, from = DUMMY_CURRENCY, to = DUMMY_CURRENCY, ratio = 0, fee_rate = 0.3) {
-    this.address = address;
+// Swap Class
+function Swap(from = DUMMY_CURRENCY, to = DUMMY_CURRENCY, ratio = DUMMY_RATIO, dex = DUMMY_DEX) {
     this.from = from;
     this.to = to;
-    this.ratio = ratio; 
-    // value : to_currency / from_currency
-    // default : 0
-    this.fee_rate = fee_rate;
-    // value : percentage
-    // default : 0.3%
+    this.ratio = ratio;
+    this.path = [from.name];
+    this.dex = dex;
 }
+const DUMMY_SWAP = new Swap();
 
-// let CurrencyLists = [
-//     new Currency('KLAY', 'au'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us'),
-//     new Currency('mary', 'us')
-// ];
-// ​
-function Market(nmae = DUMMY_MARKET, address = DUMMY_ADDRESS, pricelist = []) {
+function Market(name = DUMMY_MARKET, address = DUMMY_ADDRESS, pricelist = []) {
     this.name = name;
     this.address = address;
     this.pricelist = pricelist;
 }
 
-// var matrix = {'KLAY':{'KLAY':1, 'USDT':1.006862, 'ETH':0.000437}, 
-// 'USDT':{'KLAY':0.993184, 'USDT':1, 'ETH':0.000435}, 
-// 'ETH':{'KLAY':2288.33, 'USDT':2294.875, 'ETH':1}};
-
-// for(const i in matrix){
-//     for(const j in i){
-//         if(i != j) matrix[i][j] *= 0.997;
-//     }
-// }
-
-// for(k=0; k<3; k++){
-//     var matrix2 = {'KLAY':{'KLAY':1, 'USDT':1.006862, 'ETH':0.000437}, 
-//     'USDT':{'KLAY':0.993184, 'USDT':1, 'ETH':0.000435}, 
-//     'ETH':{'KLAY':2288.33, 'USDT':2294.875, 'ETH':1}};
-//     for(const i in matrix){
-//         for(const j in i){
-//             for(const m in i){
-//                 if(matrix[i][m]*matrix[m][j] > matrix2[i][j]) matrix2[i][j] = matrix[i][m]*matrix[m][j];
-//             }
-//         }
-//     }
-//     console.log(matrix);
-// }
-const ele = 16;
-//[WETH, CAKE, USDT]
-
-// var matrix = [
-//     [1, 153.877, 2284.51],
-//     [1/153.877, 1, 14.7963],
-//     [1/2284.51, 1/14.7963, 1]
-// ];
-
-//[BNB, BUSD, USDT]
-// var matrix = [
-//     [1, 312.113, 311.965],
-//     [1/312.113, 1, 1/1.00155],
-//     [1/311.965, 1.00155, 1]
-// ];
-
-//[WETH, CAKE, USDT]
-// var matrix = [
-//     [1, 1/0.00650912, 1/0.00043135],
-//     [0.00650912, 1, 15.14555963],
-//     [0.00043135, 1/15.14555963, 1]
-// ];
-
-var matrix = [];
-for(i=0; i<ele; i++) {
+// given that exhcange rate is given as 
+// _from _to _ratio, run the code bleow.
+var swap_matrix = [];
+for(i=0; i<MATRIX_SIZE; i++) {
     var row = [];
-    for(j=0; j<ele; j++) row.push(0);
-    matrix.push(row);
+    for(j=0; j<MATRIX_SIZE; j++) row.push(DUMMY_SWAP);
+    swap_matrix.push(row);
 }
-
-for (let i = 0; i < ele; i++) {
-    for (let j = 0; j < ele; j++) {
-        if (i == j) matrix[i][j] = 1;
-        else if (i > j) matrix[i][j] = 1 / Math.random();
+var swap_matrix2 = [];
+for(i=0; i<MATRIX_SIZE; i++) {
+    var row = [];
+    for(j=0; j<MATRIX_SIZE; j++) row.push(DUMMY_SWAP);
+    swap_matrix2.push(row);
+}
+// init
+for (let i = 0; i < MATRIX_SIZE; i++) {
+    for (let j = 0; j < MATRIX_SIZE; j++) {
+        swap_matrix[i][j] = new Swap(CurrencyLists[i], CurrencyLists[j], DUMMY_RATIO, DUMMY_DEX)
+        swap_matrix2[i][j] = new Swap(CurrencyLists[i], CurrencyLists[j], DUMMY_RATIO, DUMMY_DEX)
     }
 }
-console.log(matrix);
-for(i=0; i<ele; i++){
-    for(j=0; j<ele; j++){
-        if (matrix[i][j] == 0) matrix[i][j] = 1/matrix[j][i];
-        if(i != j) matrix[i][j] *= 0.997;
+
+// -------------------------------------------------------- for test
+// filling random ratio of lower diagonal
+for (i = 0; i < MATRIX_SIZE; i++) {
+    for (j = 0; j < MATRIX_SIZE; j++) {
+        if (i == j) swap_matrix[i][j].ratio = 1;
+        else if (i > j) swap_matrix[i][j].ratio = Math.random() * (MAX_RATIO - MIN_RATIO) + MIN_RATIO;
     }
 }
-// console.log(matrix);
 
-var matrix2 = [];
-for(i=0; i<ele; i++) {
-    var row = [];
-    for(j=0; j<ele; j++) row.push(0);
-    matrix2.push(row);
+// filling ratio of upper diagonal
+// and, should add exchange fee for now 0.3%
+// Todo : exchange fee
+for(i = 0; i < MATRIX_SIZE; i++){
+    for(j = 0; j < MATRIX_SIZE; j++){
+        if (swap_matrix[i][j].ratio == DUMMY_RATIO) swap_matrix[i][j].ratio = 1/swap_matrix[j][i].ratio;
+        else if(i != j) swap_matrix[i][j].ratio *= (1 - KLAYSWAP_FEE);
+    }
 }
+console.log(JSON.stringify(swap_matrix));
+// -------------------------------------------------------- for test
+var jsondata1 = JSON.stringify(swap_matrix,null,2);
+// jsondata.split();
+var fs = require('fs');
+fs.writeFile("initial.txt", jsondata1, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
 
-for(t=0; t<ele; t++){
-    for(i=0; i<ele; i++){
-        for(j=0; j<ele; j++){
-            for(k=0; k<ele; k++){
-                matrix2[i][k] = Math.max(matrix2[i][k], matrix[i][k], matrix[i][j]*matrix[j][k]);
+for(t=0; t<2; t++){
+    for(i=0; i<MATRIX_SIZE; i++){
+        for(j=0; j<MATRIX_SIZE; j++){
+            for(k=0; k<MATRIX_SIZE; k++){
+                // swap_matrix2[i][k].ratio = Math.max(swap_matrix[i][k].ratio, swap_matrix2[i][k].ratio, swap_matrix[i][j].ratio*swap_matrix[j][k].ratio);
+                const max_val = Math.max(swap_matrix[i][k].ratio, swap_matrix2[i][k].ratio, safemath.safeMule(swap_matrix[i][j].ratio, swap_matrix[j][k].ratio));
+                switch(max_val){
+                    case swap_matrix[i][k].ratio:
+                        swap_matrix2[i][k].path = JSON.parse(JSON.stringify(swap_matrix[i][k].path));
+                        swap_matrix2[i][k].ratio = max_val;
+                        break;
+                    case safemath.safeMule(swap_matrix[i][j].ratio, swap_matrix[j][k].ratio):
+                        swap_matrix2[i][k].path = JSON.parse(JSON.stringify(swap_matrix[i][j].path.concat(swap_matrix[j][k].path)));
+                        swap_matrix2[i][k].ratio = max_val;
+                        break;
+                }
+                if(t < 10000 && swap_matrix2[i][i].ratio > 1) {
+                    console.log("Arbitrage Opportunity", swap_matrix2[i][i].from, swap_matrix2[i][i].path, swap_matrix2[i][i].to, swap_matrix2[i][i].ratio);
+                    t=10000;
+                }
             }
         }
     }
-    if(JSON.stringify(matrix) == JSON.stringify(matrix2)) break;
-    console.log(matrix2);
-    matrix = matrix2;
+    if(JSON.stringify(swap_matrix2) == JSON.stringify(swap_matrix)) break;
+    console.log(JSON.stringify(swap_matrix2));
+    swap_matrix = JSON.parse(JSON.stringify(swap_matrix2));
 }
-// let Swaps = [
-//     new Swap(),
-//     new Swap(),
-//     new Swap(),
-//     new Swap()
-// ];
-// ​
-// let Path = [];
-// Path.push(CurrencyLists[0]);
-// Path.push(Swaps[1]);
-// Path.push(CurrencyLists[2]);
-// Path.push(Swaps[3]);
-// Path.push(CurrencyLists[1]);
-// ​
-// console.log(Path);
+
+// var jsondata = JSON.stringify(swap_matrix,null,2);
+var jsondata = '';
+for(i=0; i<MATRIX_SIZE; i++){
+    for(j=0; j<MATRIX_SIZE; j++){
+        jsondata += JSON.stringify(swap_matrix[i][j].from.name + ' ' + swap_matrix[i][j].to.name + ' ' + swap_matrix[i][j].ratio);
+    }
+    jsondata += '\n';
+}
+// jsondata.split();
+var fs = require('fs');
+fs.writeFile("result.txt", jsondata, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
