7664209912150e622fd686ea88aebf72fa49106a
leegaeun00 <leegaeun1234@minerva.kgi.edu>
2021-08-09 18:55:15 +0900

    plz

--- swap-frontend/src/App.js
+++ swap-frontend/src/App.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, {useEffect, useState} from "react";
+=======
+import React, {useState} from "react";
+>>>>>>> parent of 4220e00 (add useEffect)
 import './App.css';
 import {Input, Box} from "@material-ui/core";
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
@@ -14,8 +18,11 @@ function App() {
 
   const tokenList = require("./tokenList.json");
 
+<<<<<<< HEAD
   const [isKaikasInstalled, setIsKaikasInstalled] = useState(true);
   const [isKaikasConnected, setIsKaikasConnected] = useState(true);
+=======
+>>>>>>> parent of 4220e00 (add useEffect)
   const [myWalletAddress, setMyWalletAddress] = useState();
   const [tokenInAmount, setTokenInAmount] = useState();
   const [fromToken, setFromToken] = useState(tokenList[0]);
@@ -23,6 +30,7 @@ function App() {
 
   console.log("myWalletAddress",myWalletAddress);
   console.log("fromToken:", fromToken.label, "toToken: ", toToken.label)
+<<<<<<< HEAD
 
   const klaytn = window.klaytn;
   console.log("klaytn",klaytn);
@@ -111,15 +119,107 @@ function App() {
     console.log("tokenInAmount",tokenInAmount);
   }
 
+=======
+
+  const klaytn = window.klaytn;
+  console.log("klaytn",klaytn);
+  const caver = new Caver(klaytn);
+// const caver = new Caver(window['klaytn']);
+
+//priavteKey 가져오는거 실패
+// let privateKey;
+// klaytn.sendAsync({
+//   method: 'klay_getAccountKey',
+//   params: ["0x3A7d1D4Ec33faA64827CA8f4f17F747834121004","latest"],
+//   from: "0x3A7d1D4Ec33faA64827CA8f4f17F747834121004"
+// }, (result) => {
+//   console.log("result", privateKey)
+//   // privateKey = result.result.key.x;
+// })
+// console.log("privateKey", privateKey);
+
+  const keystore = require('./keystore.json')
+  const keyring = caver.wallet.keyring.decrypt(keystore,"leegaeun4927!");
+//add keyring to wallet
+  caver.wallet.add(keyring)
+
+//klaytn.sendAsync({method: 'klay_sendTransaction' ... 을 통해 transaction 보내야할듯
+
+  const getNetwork = async() => {
+    const network = await klaytn.networkVersion
+    if (network===8217){
+      // console.log("cypress main network")
+    } else if (network===1001){
+      // console.log("baobab test network")
+    }
+  }
+  getNetwork()
+
+  const getAccount = async() => {
+    let account;
+    account = await klaytn.selectedAddress
+    // console.log("account in getAccount()", account)
+    // klaytn.on('accountsChanged', (accounts) => {
+    //     account = accounts[0];
+    //     console.log("user changed her account to ", account)
+    // })
+    setMyWalletAddress(account);
+    return account;
+  }
+  getAccount()
+
+  const isKaikasInstalled = async() => {
+    if (klaytn.isKaikas){
+      console.log("Kaikas is installed")
+      return true;
+    } else {
+      console.log("Kaikas is NOT installed")
+      return false;
+    }
+    // 이게 위에 코드랑 뭐가 다른지 모르겠음
+    // if (typeof(klaytn) === 'undefined'){
+    //   console.log("user does not used kaikas")
+    // return false;
+    // } else {
+    //   console.log("user uses kaikas")
+    // return true;
+    // }
+  }
+
+  const isKaikasConnected = async() => {
+    // console.log("klaytn._kaikas.isEnabled()",klaytn._kaikas.isEnabled())
+    if (klaytn._kaikas.isEnabled()){
+      console.log("Kaikas is connected")
+      return true;
+    } else {
+      console.log("Kaikas is NOT connected")
+      return false;
+    }
+  }
+
+  const changeTokenInAmount = async(value) => {
+    await setTokenInAmount(value);
+    console.log("tokenInAmount",tokenInAmount);
+  }
+
+>>>>>>> parent of 4220e00 (add useEffect)
   return (
       <div className="App">
         <header className="App-header">
 
+<<<<<<< HEAD
           {!isKaikasInstalled ?
               <InstallKaikas/>
               :
               <div>
                 {!isKaikasConnected ?
+=======
+          {!isKaikasInstalled() ?
+              <InstallKaikas/>
+              :
+              <div>
+                {!isKaikasConnected() ?
+>>>>>>> parent of 4220e00 (add useEffect)
                     <ConnectKaikas klaytn={klaytn} setMyWalletAddress={setMyWalletAddress}/>
                     :
                     <Box style = {{ color: "#3A2A17", padding: "30px 30px", fontSize: "15px", backgroundColor: "#FFFDD0" }}>
@@ -148,8 +248,13 @@ function App() {
                         <div> {toToken.label} </div>
                         <SelectToken setFromOrToToken={setToToken}/>
                       </Box>
+<<<<<<< HEAD
                       <Swap caver={caver} myWalletAddress={myWalletAddress} tokenInAddress={fromToken.address}
                             tokenOutAddress={toToken.address} tokenInAmount={tokenInAmount} slippage={5}/>
+=======
+                      <Swap caver={caver} myWalletAddress={myWalletAddress} tokenInLabel={fromToken.label}
+                            tokenOutLabel={toToken.label} tokenInAmount={tokenInAmount} slippage={5}/>
+>>>>>>> parent of 4220e00 (add useEffect)
                     </Box>
                 }
               </div>
