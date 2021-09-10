63d1788be6e2110dd9ea8cf271b499126414aedf
Mingyu_Macbook <54785113+Mingyu-Lucif@users.noreply.github.com>
2021-09-10 10:10:54 +0900

    Check

--- swap-frontend/src/components/Data/shifts.js
+++ swap-frontend/src/components/Data/shifts.js
@@ -1,31 +1,31 @@
-const big = require("bignumber.js");
+const big = require('bignumber.js');
 
-async function lshift (stringint, howmany) {
-    let origin = stringint.indexOf('.');
-    if (origin == -1) stringint = stringint+'.';
-    origin = stringint.indexOf('.');
-    var zeros = []
-    for (var i = 0; i < Math.abs(howmany); i++) zeros.push('0');
-    var temp = zeros.concat(stringint.split('')).concat(zeros)
-    var neworigin = temp.indexOf('.')
-    temp.splice(neworigin, 1)
-    // small unit to large unit (peb => KLAY)
-    if (howmany > 0) {
-        temp.splice(neworigin - howmany, 0, '.')
-        temp.splice(temp.indexOf('.')+ Math.abs(howmany) + 1)
-        // return big.BigNumber(temp.join(''));
-        return temp.join('');
-    }
-    // large unit to small unit (KLAY => peb)
-    else {
-        temp.splice(neworigin - howmany, 0, '.')
-        temp.splice(temp.indexOf('.'))
-        // return big.BigNumber(temp.join(''));
-        return temp.join('');
-    }
+async function lshift(stringint, howmany) {
+	if (stringint.indexOf('.') == -1) stringint = stringint + '.';
+	let origin = stringint.indexOf('.');
+
+	let zeros = [];
+	for (var i = 0; i < Math.abs(howmany); i++) zeros.push('0');
+	let temp = zeros.concat(stringint.split('')).concat(zeros);
+	let neworigin = temp.indexOf('.');
+	temp.splice(neworigin, 1);
+	// small unit to large unit (peb => KLAY)
+	if (howmany > 0) {
+		temp.splice(neworigin - howmany, 0, '.');
+		temp.splice(temp.indexOf('.') + Math.abs(howmany) + 1);
+		// return big.BigNumber(temp.join(''));
+		return temp.join('');
+	}
+	// large unit to small unit (KLAY => peb)
+	else {
+		temp.splice(neworigin - howmany, 0, '.');
+		temp.splice(temp.indexOf('.'));
+		// return big.BigNumber(temp.join(''));
+		return temp.join('');
+	}
 }
 module.exports = {
-    lshift
-}
+	lshift,
+};
 
 // bignumber
